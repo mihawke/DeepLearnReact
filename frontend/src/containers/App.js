b3cb447e5fba9fa5625ad4ccf48c2dee0d12e29e
Pratik-22 <pratikgawari0000@gmail.com>
2023-03-21 22:08:23 +0530

    navigated to homepage

--- frontend/src/containers/App.js
+++ frontend/src/containers/App.js
@@ -1,12 +1,14 @@
 import '../styles/App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import HomePage from './HomePage';
 
 function App() {
   return (
     <Router>
-    <Routes>
-    </Routes>
-  </Router>
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+      </Routes>
+    </Router>
   );
 }
 export default App;
