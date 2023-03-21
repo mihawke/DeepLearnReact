bb3fd6cdb7d192fe2544c15509ff9fb3faeab279
Pratik-22 <pratikgawari0000@gmail.com>
2023-03-21 22:03:56 +0530

    commit

--- frontend/src/containers/App.js
+++ frontend/src/containers/App.js
@@ -1,23 +1,12 @@
-import './App.css';
+import '../styles/App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <Routes>
+    </Routes>
+  </Router>
   );
 }
-
 export default App;
