c691b04e3d6b505d23238aa1355a504d9bde7196
Pratik-22 <pratikgawari0000@gmail.com>
2023-03-21 22:22:50 +0530

    some changes

--- frontend/src/containers/HomePage.jsx
+++ frontend/src/containers/HomePage.jsx
@@ -3,7 +3,26 @@ import '../styles/HomePage.css'
 
 const HomePage = () => {
   return (
-    <div>
+    <div className='container'>
+      <div>
+        <ul className='nav-container'>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Blog</a>
+          </li>
+          <li>
+            <a href="#">Services</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
       <h1>HomePage</h1>
     </div>
   )
