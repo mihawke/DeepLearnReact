8d6b126c635f5ac218222934f47d75870ea094ba
Pratik-22 <pratikgawari0000@gmail.com>
2023-03-22 22:52:12 +0530

    added navbar to homepage

--- frontend/src/containers/HomePage.jsx
+++ frontend/src/containers/HomePage.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import './styles/HomePage.css'
 import { Cards } from '../components/cards'
+import { NavBar } from '../components/navbar'
 
 const HomePage = () => {
   return (
     <div className='container'>
       <div>
-        <Cards></Cards>
+        <NavBar/>
+      </div>
+      <div>
+        <Cards/>
       </div>
     </div>
   )
