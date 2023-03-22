c06a58441f4385afad32e5fd692b60c62ce9937e
Pratik-22 <pratikgawari0000@gmail.com>
2023-03-22 22:48:25 +0530

    Added navigation bar

---
+++ frontend/src/components/navigation.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export const NavBar = () => {
+  return (
+    <div>
+        <div>
+        <ul className='nav-container'>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Blog</a>
+          </li>
+          <li>
+            <a href="#">Services</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
+}
--- frontend/src/containers/HomePage.jsx
+++ frontend/src/containers/HomePage.jsx
@@ -6,25 +6,6 @@ const HomePage = () => {
   return (
     <div className='container'>
       <div>
-        <ul className='nav-container'>
-          <li>
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#">About</a>
-          </li>
-          <li>
-            <a href="#">Blog</a>
-          </li>
-          <li>
-            <a href="#">Services</a>
-          </li>
-          <li>
-            <a href="#">Contact</a>
-          </li>
-        </ul>
-      </div>
-      <div>
         <Cards></Cards>
       </div>
     </div>
