63b59b02664e8e31d4b259683eaa378313043c0a
Pratik-22 <pratikgawari0000@gmail.com>
2023-03-28 10:21:04 +0530

    updated

--- backend/server.py
+++ backend/server.py
@@ -1,11 +1,13 @@
-from flask import Flask, jsonify , send_file ,request
+from flask import Flask, jsonify, send_file, request 
 from pymongo import MongoClient
 from bson import json_util
 import json
 import gridfs
 import io
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
 # Connect to the MongoDB database
@@ -25,6 +27,8 @@ models = assets["models"]
 fs = gridfs.GridFS(assets)
 
 # file upload route and function
+
+
 @app.route("/upload", methods=["POST"])
 def upload_file():
     file = request.files['file']
@@ -32,6 +36,25 @@ def upload_file():
     file_id = fs.put(contents, filename=file.filename)
     images.insert_one({"file_id": file_id})
     return "File uploaded successfully!"
+
+# file retrieve route
+
+
+@app.route("/image/<filename>")
+def retrieve(filename):
+    # get_last_version is a method or function provided by the fs module that takes a filename parameter and returns the last version of the file with that name.
+    # The read method is called on the last version
+    file_data = fs.get_last_version(filename=filename).read()
+    return send_file(io.BytesIO(file_data), mimetype='image/jpeg')
+
+@app.route("/file/<filename>")
+def file(filename):
+    # retrieve the file from the database using GridFS
+    file_data = fs.get_last_version(filename=filename).read()
+
+    # use send_file() to send the file data back to the client as an attachment
+    return send_file(io.BytesIO(file_data), mimetype='image/jpeg')
+
 
 @app.route("/users")
 def print_users():
--- frontend/src/components/imageShow.jsx
+++ frontend/src/components/imageShow.jsx
@@ -1,30 +1,37 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import axios from './axios.config';
 
 const ImageList = () => {
-    const [file, setFile] = useState(null);
+  const [file, setFile] = useState(null);
+  const [fileName, setFileName] = useState(null);
 
-    useEffect(() => {
-        async function fetchFile() {
-          try {
-            const api = axios.create({
-              responseType: 'blob'
-            });
-            const response = await api.get('/file/Sekiro.jpg');
-            const blob = response.data;
-            setFile(URL.createObjectURL(blob));
-          } catch (error) {
-            console.error('Error fetching image:', error);
-          }
-        }
-        fetchFile();
-      }, []);
-  
-    return (
-      <div>
-        {file && <img src={file} alt="myfile" />}
-      </div>
-    );
+  const changeFileName = (event) => {
+    setFileName(event.target.value)
   }
+
+  async function handleSubmit(event){
+    event.preventDefault();
+    try {
+      const api = axios.create({
+        responseType: 'blob'
+      });
+      const response = await api.get(`/file/${fileName}`);
+      const blob = response.data;
+      setFile(URL.createObjectURL(blob));
+    } catch (error) {
+      console.error('Error fetching image:', error);
+    }
+  }
+
+  return (
+    <div>
+      {file && <img src={file} alt="myfile" />}
+      <form onSubmit={handleSubmit}>
+        <input id='name' type='text' onChange={changeFileName}></input>
+        <button type='submit' >Submit</button>
+      </form>
+    </div>
+  );
+}
 
 export default ImageList;
